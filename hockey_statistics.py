303eb239ea4cf81d261a71436570eed37bf4b9cf
Lucas Patriquin <lucas.patriquin@gmail.com>
2024-12-07 20:18:01 -0800

    Update hockey_statistics.py
    finished application

--- hockey_statistics.py
+++ hockey_statistics.py
@@ -17,6 +17,9 @@ class FileHandler:
             players = json.load(file)
         return players
     
+    def count_obj(self):
+        return len(self.data)
+    
         
 class ProcessData:
     '''
@@ -34,28 +37,57 @@ class ProcessData:
     def get_player_goals(self): 
         return next(player["goals"] for player in self.data if player["name"] == self.name)
         
-    def get_player_penalties(self):
-        return next(player["penalties"] for player in self.data if player["name"] == self.name)
+    def get_player_assists(self):
+        return next(player["assists"] for player in self.data if player["name"] == self.name)
     
-    # Return list of teams in data
+    # Command 2: return list of teams in data
     def get_teams(self): 
-        return[player["team"] for player in self.data]
+        return[player["team"] for player in self.data] # list comprehension
+
+        #same result as generator
+        '''
+        seen_teams = set() 
+        for player in self.data:
+            team = player['team']
+            if team not in seen_teams:
+                seen_teams.add(team)
+                yield team
+        '''
     
-    # Return list of countries in data   
+    # Command 3: return list of countries in data   
     def get_countries(self): 
         return[player["nationality"] for player in self.data]
     
-    # List players in a specific team in the order of points scored, from highest to lowest
+    # Command 4: return players in a specific team in the order of points scored, from highest to lowest
     def players_in_team(self, team): 
-        return[player["name"] for player in self.data if player["team"] == team]
-    '''Need to correct'''
+        def order_by_points(player: dict):
+            points = player["goals"] + player["assists"]
+            return points
+        
+        # List of players from team sorted by most goals scored 
+        sorted_players = sorted(self.data, key=order_by_points, reverse=True)
+        for player in sorted_players:
+            if player['team'] == team:
+                yield str(ProcessData(player["name"], self.data))
+    
+    # Command 5: list of players sorted by points for the country mentioned
+    def players_from_country(self, country):
+        def order_by_points(player: dict):
+            points = player["goals"] + player["assists"]
+            return points
+        
+        # List of players from country sorted by most goals scored 
+        sorted_players = sorted(self.data, key=order_by_points, reverse=True)
+        for player in sorted_players:
+            if player['nationality'] == country:
+                yield str(ProcessData(player["name"], self.data))
     
     # Return list of players sorted by points    
     def sort_by_most_points(self, num_players): 
         # Helper function
         def order_by_points(player: dict):
             points = player["goals"] + player["assists"]
-            return points
+            return (points, player["goals"])
         
         # Return list of top players based on num_players called
         top_players = sorted(self.data, key=order_by_points, reverse=True)[:num_players]
@@ -64,7 +96,7 @@ class ProcessData:
     def sort_by_most_goals(self, num_players):
         # Helper function
         def order_by_goals(player: dict):
-            return player["goals"]
+            return(player["goals"], -player["games"])
         
         # Return list of top players based on num_players called
         top_players = sorted(self.data, key=order_by_goals, reverse=True)[:num_players]
@@ -80,17 +112,16 @@ class ProcessData:
         name = self.name
         team = self.get_player_team()
         goals = self.get_player_goals()
-        penalties = self.get_player_penalties()
-        return f"{name:<21}{team:<5}{goals:>2} + {penalties:>2} = {goals + penalties:>2}"
+        assists = self.get_player_assists()
+        return f"{name:<21}{team:<5}{goals:>2} +{assists:>3} = {goals + assists:>3}"
     
-
 
 class HockeyDataApplication:
     '''
     Application "user interface" for processing hockey data
     '''
     def __init__(self):
-        self.__hockeydata = ProcessData(None, FileHandler.load_file)
+        self.file_data = None
     
     def help(self):
         print("commands:")
@@ -104,60 +135,120 @@ class HockeyDataApplication:
         print("7 most goals")
         
     def execute(self):
+        file_name = input('file name: ')
+        file_handler = FileHandler(file_name)
+        self.file_data = file_handler.load_file()
+        count = FileHandler(file_name).count_obj()
+        print(f'read the data of {count} players ')
+        print("")
         self.help()
-        print()
-        command = int(input("command: "))
+        while True:
+            print()
+            try:
+                command = int(input("command: "))
+                if command == 0:
+                    break
+                elif command == 1:
+                    self.player_search()
+                elif command == 2:
+                    self.teams()
+                elif command == 3:
+                    self.countries()
+                elif command == 4:
+                    self.players_in_teams()
+                elif command == 5:
+                    self.players_from_country()
+                elif command == 6:
+                    self.most_points()
+                elif command == 7:
+                    self.most_goals()
+                else:
+                    self.help()
+            except ValueError:
+                print('Please enter a numbered command between 0-7')
         
-        if command == 0:
-            quit
-        elif command == 1:
-            self.player_search()
-        elif command == 2:
-            self.teams()
-        elif command == 3:
-            self.countries()
-        elif command == 4:
-            self.players_in_teams()
-        elif command == 5:
-            self.players_from_country()
-        elif command == 6:
-            self.most_points()
-        elif command == 7:
-            self.most_goals()
-        else:
-            return # add error handling for anything not a int 0-7
+    def player_search(self): # command 1
+        name = input("name: ")
+        try:
+            player = ProcessData(name, self.file_data)
+            print(player)
+        except StopIteration:
+            print(f"Player {name} not found.")
+    
+    def teams(self): # command 2
+        data = ProcessData(None, self.file_data)
+        for team in sorted(set(data.get_teams())):
+            print(team)
+    
+    def countries(self): # command 3
+        data = ProcessData(None, self.file_data)
+        for country in sorted(set(data.get_countries())):
+            print(country)
+    
+    def players_in_teams(self): # command 4
+        team = input("team: ")
+        try:
+            print("")
+            data = ProcessData(None, self.file_data)
+            team_found = False
+            for player in data.players_in_team(team):
+                print(player)
+                team_found = True
+            if not team_found:
+                print(f"Team {team} not found.")
+        except ValueError as e:
+            print(e)
+        except Exception as e:
+            print(f"An unexpected error occurred: {e}")
+            
+    def players_from_country(self): # command 5
+        country = input("country: ")
+        try:
+            print("")
+            data = ProcessData(None, self.file_data)
+            country_found = False
+            for player in data.players_from_country(country):
+                print(player)
+                country_found = True
+            if not country_found:
+                print(f"Country {country} not found.")
+        except ValueError as e:
+            print(e)
+        except Exception as e:
+            print(f"An unexpected error occurred: {e}")
+    
+    def most_points(self): # Command 6
+        quantity = int(input("how many: "))
+        try:
+            print("")
+            data = ProcessData(None, self.file_data)
+            players_found = False
+            for player in data.sort_by_most_points(quantity):
+                print(player)
+                players_found = True
+            if not players_found:
+                print(f"Invalid entry")
+        except ValueError as e:
+            print(e)
+        except Exception as e:
+            print(f"An unexpected error occurred: {e}")
+    
+    def most_goals(self): # Command 7
+        quantity = int(input("how many: "))
+        try:
+            print("")
+            data = ProcessData(None, self.file_data)
+            players_found = False
+            for player in data.sort_by_most_goals(quantity):
+                print(player)
+                players_found = True
+            if not players_found:
+                print(f"Invalid entry")
+        except ValueError as e:
+            print(e)
+        except Exception as e:
+            print(f"An unexpected error occurred: {e}")
         
-    def player_search(self):
-        ...
-    
-    def teams(self):
-        ...
-    
-    def countries(self):
-        ...
-    
-    def players_in_teams(self):
-        ...
-    
-    def players_from_country(self):
-        ...
-    
-    def most_points(self):
-        ...
-    
-    def most_goals(self):
-        ...
         
-        
-
-'''data = FileHandler("all.json")
-processed_data = ProcessData(None, data.data)
-top_players = processed_data.sort_most_points(10)
-for player in top_players:
-    print(player)'''
-    
 app = HockeyDataApplication()
 app.execute()
-    
-# res = PlayerData("Andy Greene")
-# print(res.get_team())
